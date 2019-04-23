e8413a00f1850aa2f612e6c8df36b95643f81d47
Polarisjunior <brownasianpj@yahoo.com>
2019-04-22 17:48:31 -0700

    handcontroller?

--- PhantasiaConductor/Assets/Scripts/HandController.cs
+++ PhantasiaConductor/Assets/Scripts/HandController.cs
@@ -23,17 +23,19 @@ namespace Valve.VR.InteractionSystem
         // Update is called once per frame
         void Update()
         {
-            Debug.Log(leftHand.transform.position);
+            // Debug.Log(leftHand.transform.position);
             
             RaycastHit hit;
             if (Physics.Raycast(leftHand.transform.position, leftHand.transform.rotation * transform.forward, Mathf.Infinity, ~(1 << 2))) {
-                Debug.Log("we have hit");
+                // Debug.Log("we have hit");
             }
             // Debug.Log(leftHand.transform.eulerAngles);
             if (debugMode) {
                 Debug.DrawRay(leftHand.transform.position, leftHand.transform.rotation * transform.forward * 1000, Color.green);
                 Debug.DrawRay(rightHand.transform.position, rightHand.transform.rotation * transform.forward * 1000, Color.blue);
             }
+
+            
             
         }
 
