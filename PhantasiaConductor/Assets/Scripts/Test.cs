0dc476546ad58266ea9b52b3c2f4a70816a74923
Polarisjunior <brownasianpj@yahoo.com>
2019-04-23 13:54:29 -0700

    test file

---
+++ PhantasiaConductor/Assets/Scripts/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// temporary code for testing small details
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnBlinkOn() {
+        Debug.Log("blinked on");
+    }
+}
