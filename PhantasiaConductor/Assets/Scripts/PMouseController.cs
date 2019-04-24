7066196d05ed1caf2ad2cd3a0f8c55a2ecc22428
Polarisjunior <brownasianpj@yahoo.com>
2019-04-24 13:42:58 -0700

    test mouse controller
    Former-commit-id: 2e488c6e1e6a1fa7f66a3198319f2917d4d8e8c1

---
+++ PhantasiaConductor/Assets/Scripts/PMouseController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PMouseController : MonoBehaviour
+{
+    public Camera playerCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        { // if left button pressed...
+            Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ~(1 << 2)))
+            {
+                Collider collider = hit.collider;
+                collider.SendMessage("OnHit", SendMessageOptions.DontRequireReceiver);
+            }
+        }
+    }
+}
