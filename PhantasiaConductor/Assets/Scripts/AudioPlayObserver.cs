86d25112a076ec6ce4ba1f6e16c752258e024ba9
Polarisjunior <brownasianpj@yahoo.com>
2019-04-25 13:45:04 -0700

    observer system
    Former-commit-id: cfad0fa772f4d96d975b5f8e34f7c34cc2e17048

---
+++ PhantasiaConductor/Assets/Scripts/AudioPlayObserver.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayObserver : PObserver
+{
+
+    public AudioSource source;
+    
+
+    public override void Run(string e) {
+        source.Play();
+    }
+}
---
+++ PhantasiaConductor/Assets/Scripts/PObserver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PObserver : MonoBehaviour
+{
+
+    private List<GameObject> subscribers;
+
+    public void Subscribe(GameObject g)
+    {
+        if (!subscribers.Contains(g))
+        {
+            subscribers.Add(g);
+        }
+    }
+
+    public void TriggerObserver(string e)
+    {
+        Debug.Log("triggered " + e);
+    }
+
+    public abstract void Run(string e); 
+}
