140f0a70d79f615ea4422c176deda294878f67b5
Polarisjunior <brownasianpj@yahoo.com>
2019-04-25 14:22:03 -0700

    audio source loop

---
+++ PhantasiaConductor/Assets/Scripts/Observers/AudioLoopObserver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioLoopObserver : PObserver
+{
+    public AudioSource source;
+
+    public void Start() {
+        source.loop = true;
+    }
+
+    public override void Run(string e) {
+        source.Play();
+    }
+}
