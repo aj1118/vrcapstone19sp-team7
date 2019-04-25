6fb6592e14d4cbf05b3627e38a9a08d8042d902e
Polarisjunior <brownasianpj@yahoo.com>
2019-04-25 14:22:03 -0700

    audio source loop
    Former-commit-id: 140f0a70d79f615ea4422c176deda294878f67b5

---
+++ PhantasiaConductor/Assets/Scripts/Audio/AudioSourceLoop.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourceLoop : MonoBehaviour
+{
+    public AudioSource source;
+
+    public float delay = 0;
+    // Start is called before the first frame update
+
+    public bool isPlaying;
+
+    public void PlayOnce()
+    {
+        source.Play();
+    }
+
+    public void PlayLooping()
+    {
+        InvokeRepeating("PlayOnce", 0, delay);
+    }
+
+    public void StopLooping()
+    {
+        CancelInvoke();
+    }
+
+    public float Delay {
+        get {
+            return delay;
+        }
+
+        set {
+            if (isPlaying) {
+                StopLooping();
+                delay = value;
+                PlayLooping();
+            }
+        }
+    }
+}
---
+++ PhantasiaConductor/Assets/Scripts/Observers/DestroyObserver.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObserver : PObserver
+{
+    public override void Run(string e) {
+        Destroy(gameObject);
+    }
+}
