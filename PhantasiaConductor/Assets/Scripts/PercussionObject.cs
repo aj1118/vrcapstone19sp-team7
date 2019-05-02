8f3c5f5bb4d14eb13609eaf30a77fcb6e74b1e12
Polarisjunior <brownasianpj@yahoo.com>
2019-05-02 16:58:59 -0700

    perc

---
+++ PhantasiaConductor/Assets/Scripts/PercussionObject.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PercussionObject : MonoBehaviour
+{
+    public AudioClip audioClip;
+    // Start is called before the first frame update
+
+    private BeatBlinkController beatBlinkController;
+
+    private AudioSource audioSource;
+
+    private AudioSource audioSourceLoop;
+
+    private AudioSourceLoop audioLoop;
+
+    private Hittable hittable;
+
+    private BeatInfo beatInfo;
+
+    void Awake()
+    {
+        beatBlinkController = GetComponent<BeatBlinkController>();
+
+        audioSource = transform.Find("AudioSource").GetComponent<AudioSource>();
+        audioSourceLoop = transform.Find("AudioSourceLoop").GetComponent<AudioSource>();
+        audioLoop = transform.Find("AudioLoop").GetComponent<AudioSourceLoop>();
+
+        audioSource.clip = audioClip;
+        audioSourceLoop.clip = audioClip;
+
+        hittable = GetComponent<Hittable>();
+        beatInfo = GetComponent<BeatInfo>();
+
+        audioLoop.beatInfo = beatInfo;
+        audioLoop.source = audioSourceLoop;
+
+        beatBlinkController.beatInfo = beatInfo;
+    }
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ PhantasiaConductor/Assets/Scripts/PercussionObjectFactory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PercussionObjectFactory : MonoBehaviour
+{
+    public List<AudioClip> audioClips;
+
+    public List<BeatInfo> beatInfos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
