649eb061242a237cd53d390d7112a03e8b985f87
V. R. Team <hwerbel@uw.edu>
2019-05-05 18:05:28 -0700

    working on harmony targets

--- PhantasiaConductor/Assets/Scripts/ChordController.cs
+++ PhantasiaConductor/Assets/Scripts/ChordController.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ChordController : MonoBehaviour
 {
-    public int numTargets;
     public GameObject[] targets;
 
+    public UnityEvent onCompleteChord;
+
     private BeatInfo beatInfo;
-    private int noteIndex = 0;
     private int beatIndex = 0;
+    private int targetIndex = 0;
+    private bool waitForLoop = false;
     private bool notePlaying = false;
 
     private void Awake()
@@ -29,14 +32,28 @@ public class ChordController : MonoBehaviour
 
     private void RunTargets()
     {
-        bool nextBeat = beatInfo.beats[(beatIndex) % beatInfo.beats.Length];
-        int noteLength = beatInfo.notes[(beatIndex) % beatInfo.notes.Length];
+        if (waitForLoop)
+        {
+            if (beatIndex != 0)
+            {
+                return;
+            } else
+            {
+                waitForLoop = false;
+            }
+        }
+
+        Debug.Log(beatIndex);
+
+        bool nextBeat = beatInfo.beats[(beatIndex + 1) % beatInfo.beats.Length];
+        int noteLength = beatInfo.notes[(beatIndex + 1) % beatInfo.notes.Length];
 
         if (nextBeat)
         {
             if (!notePlaying)
             {
-                // make target glow
+                // Start glowing target
+                targets[targetIndex].transform.Find("TargetObject").GetComponent<Renderer>().enabled = true;
             }
             if (noteLength > 1)
             {
@@ -49,6 +66,14 @@ public class ChordController : MonoBehaviour
         else
         {
             // Stop glowing target 
+            targets[targetIndex].transform.Find("TargetObject").GetComponent<Renderer>().enabled = false;
+            targetIndex++;
+        }
+
+        // May need to move this to begining of method
+        if (targetIndex == targets.Length)
+        {
+            onCompleteChord.Invoke();
         }
 
         beatIndex++;
@@ -57,5 +82,11 @@ public class ChordController : MonoBehaviour
         {
             Invoke("RunTarget", beatInfo.timePerBeat);
         }
+    }
+
+    public void ResetTargets()
+    {
+        targetIndex = 0;
+        waitForLoop = true;
     }
 }
