e677546da42038334da0042d378843badc2952bf
PolarisJunior <brownasianpj@yahoo.com>
2019-05-06 00:17:57 -0700

    puzzle monitor state

---
+++ PhantasiaConductor/Assets/Scripts/PuzzleMonitor.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events;
+
+public class PuzzleMonitor : MonoBehaviour
+{
+    public UnityEvent onPuzzleCompleted;
+
+    public List<UnityEvent> puzzleEvents;
+
+    Dictionary<UnityEvent, bool> puzzleState;
+
+    float timeElapsed = 0;
+    bool timerStarted = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var e in puzzleEvents) {
+            puzzleState[e] = false;
+        }
+    }
+
+    void Update()
+    {
+        if (timerStarted)
+        {
+            timeElapsed += Time.deltaTime;
+        }
+    }
+
+    public void StartTimer()
+    {
+        timerStarted = true;
+    }
+
+    public void StopTimer()
+    {
+        timerStarted = false;
+    }
+
+    public void ResetTimer()
+    {
+        timeElapsed = 0;
+    }
+
+    public void Reset()
+    {
+        foreach (var pair in puzzleState)
+        {
+            puzzleState[pair.Key] = false;
+            CheckCompleted();
+        }
+    }
+
+    public void Register(UnityEvent e)
+    {
+        puzzleState.Add(e, false);
+        e.AddListener(delegate ()
+        {
+            puzzleState[e] = true;
+        });
+    }
+
+    public void CheckCompleted()
+    {
+        if (puzzleCompleted) {
+            onPuzzleCompleted.Invoke();
+        }
+    }
+
+    public bool puzzleCompleted
+    {
+        get
+        {
+            foreach (var pair in puzzleState)
+            {
+                if (!pair.Value)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
