37e4193dfd92c4bce5d8c80b393eb24f115d3570
V. R. Team <hwerbel@uw.edu>
2019-05-07 18:13:24 -0700

    refine teleportation

---
+++ PhantasiaConductor/Assets/Scripts/Glow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem
+{
+    public class Glow : MonoBehaviour
+    {
+        public Material glowMaterial;
+
+        private Material originalMaterial;
+
+        void Awake()
+        {
+
+            originalMaterial = GetComponent<Renderer>().material;
+        }
+
+        public void GlowOn()
+        {
+            GetComponent<Renderer>().material = glowMaterial;
+        }
+
+        public void GlowOff()
+        {
+            GetComponent<Renderer>().material = originalMaterial;
+        }
+    }
+}
