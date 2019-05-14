466c503fe6837506fc9b39032154f4870d3131b4
bencelsi <benjamin.d.celsi@gmail.com>
2019-05-13 18:15:26 -0700

    Finished rhythm puzzle prefab demo

--- PhantasiaConductor/Assets/HarmonyHand.cs
+++ PhantasiaConductor/Assets/HarmonyHand.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class HarmonyHand : MonoBehaviour
 {
+    //NOTE: The harmony puzzle requires that this object is the collider, and the normal hand object is not a collider. I've been using the prefab object SphereHandNoCollider.
 
 	public GameObject hand;
 	public bool unlocked;
+    private void Awake()
+    {
+        //hand.GetComponent<Collider>().enabled = false;
+    }
+
     // Update is called once per frame
-	void Update()
+    void Update()
 	{
-		GetComponent<Renderer>().material.color = Color.HSVToRGB(transform.position.y % 1f,1f,1f);
-		if (!unlocked) {
-			transform.position = new Vector3(0, hand.transform.position.y, 0);
-		}
+		if (unlocked) {
+            GetComponent<Renderer>().material.color = Color.HSVToRGB(transform.position.y % 1f, .7f, 1f);
+            
+            //Cool effect.. but nauseating!
+            //transform.rotation = Quaternion.Euler(-90, transform.position.y % 1f * 180, 0);
+        } else
+        {
+            GetComponent<Renderer>().material.color = Color.HSVToRGB(transform.position.y % 1f, .3f, 1f);
+            transform.position = new Vector3(0, hand.transform.position.y, 0);
+        }
 	}
 }
