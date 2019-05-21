dcf8c17e4ebc532a1c739b1f5c7a0ae098e24be8
aj1118 <ajkruse1118@gmail.com>
2019-05-21 15:36:00 -0700

    radial prefab and object

---
+++ PhantasiaConductor/Assets/Scripts/RadialPuzzle/RadialObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events;
+
+public class RadialObject : MonoBehaviour
+{
+    public UnityEvent onSuccess;
+
+    public UnityEvent onFailed;
+     
+
+    public float lifetime = 4f;
+
+    private float acc = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        acc += Time.deltaTime;
+        if (acc >= lifetime)
+        {
+            onFailed.Invoke();
+            Destroy(gameObject);
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        onSuccess.Invoke();
+        Destroy(gameObject);
+    }
+
+    void Fadeout()
+    {
+
+    }
+}
