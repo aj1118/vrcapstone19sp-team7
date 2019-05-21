a7e9d6a9ad94b6ff8761a787ee6a19c6aef1abf7
PolarisJunior <brownasianpj@yahoo.com>
2019-05-21 00:48:43 -0700

    radial puzzle stuff

---
+++ PhantasiaConductor/Assets/Scripts/RadialPuzzle/RadialController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadialController : MonoBehaviour
+{
+
+    public float radius = 10f;
+
+    public GameObject objPrefab;
+
+    private GameObject obj;
+
+    private bool mouseMode = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // obj = Instantiate(objPrefab);
+        obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        obj.transform.parent = transform;
+    }
+
+    void Update()
+    {
+        if (mouseMode)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            ray.direction = new Vector3(ray.direction.x, 0, ray.direction.z).normalized;
+            ray.origin = transform.position;
+            Vector3 pos = ray.origin + (ray.direction * radius);
+
+            obj.transform.position = new Vector3(pos.x, transform.position.y, pos.z);
+        }
+
+    }
+}
---
+++ PhantasiaConductor/Assets/Scripts/RadialPuzzle/RadialSequence.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadialSequence : MonoBehaviour
+{
+
+    public BeatInfo[] beatInfos;
+
+    public float[] spawnDegrees;
+
+    public MasterLoop masterLoop;
+
+    public GameObject radialObjectPrefab;
+
+    public float radius = 10f;
+    public float spawnHeight = 5f;
+
+    private int beatInfoIndex = 0;
+    private int beatIndex = 0;
+    private int rIndex = 0;
+
+    private float timePerBeat;
+
+    private float degOffset = 90;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timePerBeat = loopTime / beatInfos[0].beats.Length;
+    }
+
+    void NextBeat()
+    {
+        BeatInfo currBeatInfo = beatInfos[beatInfoIndex];
+        bool shouldSpawn = currBeatInfo.beats[beatIndex];
+
+        if (shouldSpawn)
+        {
+            GameObject obj = Instantiate(radialObjectPrefab);
+            obj.transform.parent = transform.parent;
+
+            float deg = spawnDegrees[rIndex] + degOffset;
+            float x = Mathf.Cos(deg * Mathf.Deg2Rad) * radius;
+            float z = Mathf.Sin(deg * Mathf.Deg2Rad) * radius;
+            obj.transform.localPosition = new Vector3(x, spawnHeight, z);
+
+            rIndex++;
+        }
+
+        beatIndex++;
+        if (beatIndex >= currBeatInfo.beats.Length)
+        {
+            beatIndex = 0;
+            beatInfoIndex++;
+        }
+
+        if (beatInfoIndex < beatInfos.Length)
+        {
+            Invoke("NextBeat", timePerBeat);
+        }
+    }
+
+    public void NewLoop()
+    {
+        if (beatInfoIndex >= beatInfos.Length)
+        {
+            rIndex = 0;
+            beatIndex = 0;
+            beatInfoIndex = 0;
+
+            CancelInvoke();
+            Invoke("NextBeat", timePerBeat);
+        }
+    }
+
+    void OnEnable()
+    {
+        // allows new loop to start the beat sequence
+        beatInfoIndex = beatInfos.Length;
+    }
+
+    float loopTime
+    {
+        get
+        {
+            return MasterLoop.loopTime;
+        }
+    }
+}
